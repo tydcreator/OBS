847ad6db6fa9957bf24578e4909bb9989eb01106
Sean DuBois <duboisea@amazon.com>
2024-06-17 14:38:27 -0400

    obs-webrtc: Add Simulcast Support

--- plugins/obs-webrtc/whip-output.h
+++ plugins/obs-webrtc/whip-output.h
@@ -13,6 +13,14 @@
 
 #include <rtc/rtc.hpp>
 
+struct videoLayerState {
+	uint16_t sequenceNumber;
+	uint32_t rtpTimestamp;
+	int64_t lastVideoTimestamp;
+	uint32_t ssrc;
+	std::string rid;
+};
+
 class WHIPOutput {
 public:
 	WHIPOutput(obs_data_t *settings, obs_output_t *output);
@@ -39,6 +47,7 @@ private:
 	void StopThread(bool signal);
 	void ParseLinkHeader(std::string linkHeader,
 			     std::vector<rtc::IceServer> &iceServers);
+	size_t CountSimulcastLayers(std::string answer);
 
 	void Send(void *data, uintptr_t size, uint64_t duration,
 		  std::shared_ptr<rtc::Track> track,
@@ -62,11 +71,13 @@ private:
 	std::shared_ptr<rtc::RtcpSrReporter> audio_sr_reporter;
 	std::shared_ptr<rtc::RtcpSrReporter> video_sr_reporter;
 
+	std::map<obs_encoder_t *, std::shared_ptr<videoLayerState>>
+		videoLayerStates;
+
 	std::atomic<size_t> total_bytes_sent;
 	std::atomic<int> connect_time_ms;
 	int64_t start_time_ns;
 	int64_t last_audio_timestamp;
-	int64_t last_video_timestamp;
 };
 
 void register_whip_output();
