4f07df8e3bae7d49bcab49ef505d24596f7d2d85
Dennis Sädtler <saedtler@twitch.tv>
2024-09-23 12:43:56 +0200

    UI: Remove multitrack deactivate handlers
    With the encoder reference counting fixups this is no longer required.

--- UI/multitrack-video-output.cpp
+++ UI/multitrack-video-output.cpp
@@ -290,7 +290,7 @@ static OBSOutputs SetupOBSOutput(QWidget *parent, const QString &multitrack_vide
 				 const char *audio_encoder_id, size_t main_audio_mixer,
 				 std::optional<size_t> vod_track_mixer);
 static void SetupSignalHandlers(bool recording, MultitrackVideoOutput *self, obs_output_t *output, OBSSignal &start,
-				OBSSignal &stop, OBSSignal &deactivate);
+				OBSSignal &stop);
 
 void MultitrackVideoOutput::PrepareStreaming(
 	QWidget *parent, const char *service_name, obs_service_t *service, const std::optional<std::string> &rtmp_url,
@@ -408,15 +408,12 @@ void MultitrackVideoOutput::PrepareStreaming(
 
 	OBSSignal start_streaming;
 	OBSSignal stop_streaming;
-	OBSSignal deactivate_stream;
-	SetupSignalHandlers(false, this, output, start_streaming, stop_streaming, deactivate_stream);
+	SetupSignalHandlers(false, this, output, start_streaming, stop_streaming);
 
 	if (dump_stream_to_file_config && recording_output) {
 		OBSSignal start_recording;
 		OBSSignal stop_recording;
-		OBSSignal deactivate_recording;
-		SetupSignalHandlers(true, this, recording_output, start_recording, stop_recording,
-				    deactivate_recording);
+		SetupSignalHandlers(true, this, recording_output, start_recording, stop_recording);
 
 		decltype(audio_encoders) recording_audio_encoders;
 		recording_audio_encoders.reserve(audio_encoders.size());
@@ -433,7 +430,6 @@ void MultitrackVideoOutput::PrepareStreaming(
 				nullptr,
 				std::move(start_recording),
 				std::move(stop_recording),
-				std::move(deactivate_recording),
 			});
 		}
 	}
@@ -446,7 +442,6 @@ void MultitrackVideoOutput::PrepareStreaming(
 		std::move(multitrack_video_service),
 		std::move(start_streaming),
 		std::move(stop_streaming),
-		std::move(deactivate_stream),
 	});
 }
 
@@ -780,7 +775,7 @@ static OBSOutputs SetupOBSOutput(QWidget *parent, const QString &multitrack_vide
 }
 
 void SetupSignalHandlers(bool recording, MultitrackVideoOutput *self, obs_output_t *output, OBSSignal &start,
-			 OBSSignal &stop, OBSSignal &deactivate)
+			 OBSSignal &stop)
 {
 	auto handler = obs_output_get_signal_handler(output);
 
@@ -788,9 +783,6 @@ void SetupSignalHandlers(bool recording, MultitrackVideoOutput *self, obs_output
 		start.Connect(handler, "start", RecordingStartHandler, self);
 
 	stop.Connect(handler, "stop", !recording ? StreamStopHandler : RecordingStopHandler, self);
-
-	deactivate.Connect(handler, "deactivate", !recording ? StreamDeactivateHandler : RecordingDeactivateHandler,
-			   self);
 }
 
 std::optional<MultitrackVideoOutput::OBSOutputObjects> MultitrackVideoOutput::take_current()
@@ -819,7 +811,7 @@ void MultitrackVideoOutput::ReleaseOnMainThread(std::optional<OBSOutputObjects> 
 		QApplication::instance()->thread(), [objects = std::move(objects)] {}, Qt::QueuedConnection);
 }
 
-void StreamStopHandler(void *arg, calldata_t *params)
+void StreamStopHandler(void *arg, calldata_t *data)
 {
 	auto self = static_cast<MultitrackVideoOutput *>(arg);
 
@@ -832,25 +824,9 @@ void StreamStopHandler(void *arg, calldata_t *params)
 	if (stream_dump_output)
 		obs_output_stop(stream_dump_output);
 
-	if (obs_output_active(static_cast<obs_output_t *>(calldata_ptr(params, "output"))))
-		return;
-
-	MultitrackVideoOutput::ReleaseOnMainThread(self->take_current());
-}
-
-void StreamDeactivateHandler(void *arg, calldata_t *params)
-{
-	auto self = static_cast<MultitrackVideoOutput *>(arg);
-
-	if (obs_output_reconnecting(static_cast<obs_output_t *>(calldata_ptr(params, "output"))))
-		return;
-
-	/* Unregister the BPM (Broadcast Performance Metrics) callback
-	 * and destroy the allocated metrics data.
-	 */
-	obs_output_remove_packet_callback(static_cast<obs_output_t *>(calldata_ptr(params, "output")), bpm_inject,
-					  NULL);
-	bpm_destroy(static_cast<obs_output_t *>(calldata_ptr(params, "output")));
+	/* Unregister the BPM (Broadcast Performance Metrics) callback and destroy the allocated metrics data. */
+	obs_output_remove_packet_callback(static_cast<obs_output_t *>(calldata_ptr(data, "output")), bpm_inject, NULL);
+	bpm_destroy(static_cast<obs_output_t *>(calldata_ptr(data, "output")));
 
 	MultitrackVideoOutput::ReleaseOnMainThread(self->take_current());
 }
@@ -860,19 +836,9 @@ void RecordingStartHandler(void * /* arg */, calldata_t * /* data */)
 	blog(LOG_INFO, "MultitrackVideoOutput: recording started");
 }
 
-void RecordingStopHandler(void *arg, calldata_t *params)
+void RecordingStopHandler(void *arg, calldata_t *)
 {
 	auto self = static_cast<MultitrackVideoOutput *>(arg);
 	blog(LOG_INFO, "MultitrackVideoOutput: recording stopped");
-
-	if (obs_output_active(static_cast<obs_output_t *>(calldata_ptr(params, "output"))))
-		return;
-
-	MultitrackVideoOutput::ReleaseOnMainThread(self->take_current_stream_dump());
-}
-
-void RecordingDeactivateHandler(void *arg, calldata_t * /*data*/)
-{
-	auto self = static_cast<MultitrackVideoOutput *>(arg);
 	MultitrackVideoOutput::ReleaseOnMainThread(self->take_current_stream_dump());
 }
