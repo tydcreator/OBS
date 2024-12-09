f05f1df5bf1eefe6da2603c37339afb246a0fb50
PatTheMav <PatTheMav@users.noreply.github.com>
2024-12-09 21:13:03 +0100

    frontend: Migrate CMake files

--- frontend/cmake/feature-whatsnew.cmake
+++ frontend/cmake/feature-whatsnew.cmake
@@ -9,7 +9,7 @@ if(ENABLE_WHATSNEW AND TARGET OBS::browser-panels)
     set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
     find_package(MbedTLS REQUIRED)
     set(CMAKE_FIND_PACKAGE_PREFER_CONFIG FALSE)
-    find_package(nlohmann_json 3.11 REQUIRED)
+    find_package(nlohmann_json REQUIRED)
 
     if(NOT TARGET OBS::blake2)
       add_subdirectory("${CMAKE_SOURCE_DIR}/deps/blake2" "${CMAKE_BINARY_DIR}/deps/blake2")
@@ -20,13 +20,15 @@ if(ENABLE_WHATSNEW AND TARGET OBS::browser-panels)
     target_sources(
       obs-studio
       PRIVATE
-        update/crypto-helpers-mbedtls.cpp
-        update/crypto-helpers.hpp
-        update/models/whatsnew.hpp
-        update/shared-update.cpp
-        update/shared-update.hpp
-        update/update-helpers.cpp
-        update/update-helpers.hpp
+        utility/crypto-helpers-mbedtls.cpp
+        utility/crypto-helpers.hpp
+        utility/models/whatsnew.hpp
+        utility/update-helpers.cpp
+        utility/update-helpers.hpp
+        utility/WhatsNewBrowserInitThread.cpp
+        utility/WhatsNewBrowserInitThread.hpp
+        utility/WhatsNewInfoThread.cpp
+        utility/WhatsNewInfoThread.hpp
     )
   endif()
 
